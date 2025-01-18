6ed82a2cc3a219a5fa82684bce8e3790313974dd
mukashova8 <alisha_mukashova_056@mail.ru>
2025-01-18 09:37:03 +0500

    Добавьте файлы проекта.

---
+++ task_02_02/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace task_02_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Найти значение выражения при a = 8, b = 14, с = π / 4
+            double a = 8;
+            double b = 14;
+            double c = Math.PI / 4;
+            double f = (Math.Pow(b + Math.Pow(a - 1, 1 / 3), 1 / 4)) / (Math.Abs(a - b) * (Math.Sin(Math.Pow(c,2))) + Math.Tan(c));
+            Console.WriteLine(f);
+        }
+    }
+}
---
+++ task_02_04/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace task_02_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Пользователь вводит свою дату рождения построчно(сначала год, потом месяц и в конце дату)
+            //произведите//расчет является ли пользователь совершеннолетним на текущую дату и выведите соответствующее сообщение обэтом
+            Console.WriteLine("Введите нынешний год :"); 
+            int y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите нынешний месяц :"); 
+            int z = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите нынешний день :"); 
+            int m = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите год своего рождения :");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите месяц своего рождения :");
+            int b = Convert.ToInt32(Console.ReadLine()); 
+            Console.WriteLine("Введите день своего рождения :");
+            int c = Convert.ToInt32(Console.ReadLine());
+            int f = y - a; 
+            if ((z < b & f == 18) || (z == b & m < c))
+            { 
+                f -= 1; 
+            }
+            Console.WriteLine(f);
+            if (f >= 18)
+            {
+                Console.WriteLine("Совершеннолетний");
+            }
+            else 
+            { 
+                Console.WriteLine("Несовершеннолетний"); 
+            }
+        }
+    }
+}
+        
