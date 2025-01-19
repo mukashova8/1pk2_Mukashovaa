3b9148c408b01942edc1d1c49592d8b45e89da77
mukashova8 <alisha_mukashova_056@mail.ru>
2025-01-19 15:34:40 +0500

    4 задача 3 урока

---
+++ task_03_04/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace task_03_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст илии exit для выхода");
+            string t;
+            int c = 0;
+            while (true)
+            {       
+                t = Console.ReadLine();
+                Console.Clear();
+                if (t.ToLower() == "exit" || t == "")
+                {
+                    break;
+                }
+                c +=1;
+            }
+            Console.WriteLine($"Количество введенных строк: {c}"); 
+            Console.ReadKey();  
+        }
+    }
+}
