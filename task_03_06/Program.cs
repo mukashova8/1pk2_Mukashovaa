e048d83ba6512703a6e4f877c4b7182b6dc04e4d
mukashova8 <alisha_mukashova_056@mail.ru>
2025-01-21 20:26:12 +0500

    добавила комментраии с условием задачи

---
+++ task_03_06/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace task_03_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Написать программу, которая выводит таблицу значений функции: 𝑦=|𝑥|для -4≤x≤4, с шагом h = 0,5.
+            Console.WriteLine("таблица значений");
+            for (double x = -4; x <= 4; x = x + 0.5)
+            {
+                double y = Math.Abs(x);
+                Console.WriteLine($"{x} \t {y}");
+            }
+
+
+
+
+        }
+    }
+}
