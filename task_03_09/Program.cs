de539d66df0ee7194345b1a0607360690035c0f8
mukashova8 <alisha_mukashova_056@mail.ru>
2025-01-21 21:00:57 +0500

    задание 9 урок 3

---
+++ task_03_09/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace task_03_09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Вклад в банке составляет x рублей. Ежегодно он увеличивается на p процентов, после чего дробная часть копеек
+            //отбрасывается.Каждый год сумма вклада становится больше. Определите, через сколько лет вклад составит не
+            //менее y рублей.
+
+            double x = Convert.ToDouble(Console.ReadLine());
+            double p = Convert.ToDouble(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+            int w = 0;
+            while (x<=y)
+            {
+                x = x + (x / 100 * p);
+                x = Math.Truncate(x);
+                w = w + 1;
+            }
+            Console.WriteLine(w);
+
+        }
+    }
+}
