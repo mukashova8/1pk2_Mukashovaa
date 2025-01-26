c166b60477c1a167381a4dc4cc4143f5915a29da
mukashova8 <alisha_mukashova_056@mail.ru>
2025-01-26 17:34:39 +0500

    задача 10 урока 4

---
+++ task_04_10/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace task_04_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Заполнить массив из 10 элементов случайными числами в интервале [-10..10] и сделать реверс элементов
+            //отдельно для 1 - ой и 2 - ой половин массива. Реверс реализовать через цикл. Стандартные методы класса Array
+            //использовать нельзя.
+
+            //создаем массив
+            int size = 10;
+            int[] arrey = new int[size];
+            Random rdn = new Random();
+            
+            for (int i = 0; i < size; i++)
+            {
+                arrey[i] = rdn.Next(-10, 10);
+                Console.Write( arrey[i] + " ");
+            }
+
+            //реверс первой половины массива 
+            int l = 0;
+            int r = arrey.Length / 2 - 1;
+            while (l < r)
+            {
+                int temp = arrey[l];
+                arrey[l] = arrey[r];
+                arrey[r] = temp;
+                l++;
+                r--;
+            }
+            //реверс второй половины массива
+            l = arrey.Length / 2;
+            r = arrey.Length - 1;
+            while (l < r)
+            {
+                int temp = arrey[l];
+                arrey[l] = arrey[r];
+                arrey[r] = temp;
+                l++;
+                r--;
+            }
+            Console.WriteLine();
+            foreach (var item in arrey)
+            {
+                
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
