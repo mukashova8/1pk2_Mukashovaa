e27337cc1461fe9372da158b1eb08afda40381a1
mukashova8 <alisha_mukashova_056@mail.ru>
2025-01-30 23:59:33 +0500

    задачи из 5 урока

--- task_05_07/Program.cs
+++ task_05_07/Program.cs
@@ -9,32 +9,101 @@
             //Найти и вывести отдельно минимальный элемент в матрице(LINQ под запретом) Осуществить умножение матрицы на ее
             //минимальный элемент, при выводе цветом выделить пять максимальных значений в массиве
 
-            int n, min = 100, max = 0;
+            int n, min = 100;
             while (!int.TryParse(Console.ReadLine(), out n))
             {
-                Console.WriteLine("ошибка, введите снова");
+                Console.WriteLine("Ошибка, введите снова");
             }
-            int[,] arrey = new int[n, n];
+
+            int[,] array = new int[n, n];
             Random rnd = new Random();
+
+            //Генерация матрицы и нахождение минимального элемента
             for (int i = 0; i < n; i++)
             {
                 for (int j = 0; j < n; j++)
                 {
-                    arrey[i, j] = rnd.Next(10, 100);
-                    if (arrey[i, j] < min)
+                    array[i, j] = rnd.Next(10, 100);
+                    if (array[i, j] < min)
                     {
-                        min = arrey[i, j];
+                        min = array[i, j];
                     }
-                    if (arrey[i, j] > max)
-                    {
-                        max = arrey[i, j];
-                    }
-                    Console.Write(arrey[i, j] + "\t");
+                    Console.Write(array[i, j] + "\t");
                 }
                 Console.WriteLine();
             }
-            Console.WriteLine(min);
 
-        }    
+            Console.WriteLine($"Минимальный элемент: {min}");
+
+            //Умножение матрицы на минимальный элемент
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    array[i, j] *= min;
+                }
+            }
+
+            //Нахождение пяти максимальных элементов вручную
+            int[] maxValues = new int[5];
+            for (int k = 0; k < 5; k++)
+            {
+                int max = int.MinValue;
+
+            //Поиск максимального элемента, который еще не был найден
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        bool isAlreadyIncluded = false;
+
+            //Проверяем, есть ли элемент в массиве maxValues
+                        for (int m = 0; m < k; m++)
+                        {
+                            if (array[i, j] == maxValues[m])
+                            {
+                                isAlreadyIncluded = true;
+                                break;
+                            }
+                        }
+
+                        if (!isAlreadyIncluded && array[i, j] > max)
+                        {
+                            max = array[i, j];
+                        }
+                    }
+                }
+
+                maxValues[k] = max; //Сохраняем найденный максимум
+            }
+
+            Console.WriteLine("Матрица после умножения:");
+            //Вывод матрицы с выделением пяти максимальных элементов
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    bool isHighlighted = false;
+
+            //Проверяем, нужно ли выделить текущий элемент
+                    for (int k = 0; k < 5; k++)
+                    {
+                        if (array[i, j] == maxValues[k])
+                        {
+                            isHighlighted = true;
+                            break;
+                        }
+                    }
+
+                    if (isHighlighted)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red; //Цвет для максимальных значений
+                    }
+                    Console.Write(array[i, j] + "\t");
+                    Console.ResetColor();
+                }
+                Console.WriteLine();
+            }
+        }   
     }
 }
---
+++ task_05_08/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace task_05_08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Даны два массива размером 10*10, заполненные целыми числами в диапазоне от 1 до 9 вкл. Создать новый массив, который будет
+            //произведением двух предыдущих(перемножить элементы первых двух массивов, стоящие на одинаковых позициях и записать их в
+            //результирующий массив)
+            //Вывести результирующий массив
+
+
+            int[,] arrey1 = new int[10,10];
+            int[,] arrey2 = new int[10,10];
+            int[,] arrey3 = new int[10, 10];
+            Random rnd = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    arrey1[i, j] = rnd.Next(1,10);
+                    arrey2[i, j] = rnd.Next(1, 10);
+                }
+            }
+            Console.WriteLine("первый массив: ");
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.Write(arrey1[i,j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("второй массив: ");
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.Write(arrey2[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            //перемножаем элементы первых двух массивов, стоящие на одинаковых позициях
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    arrey3[i, j] = arrey1[i, j] * arrey2[i,j];
+                }
+            }
+            Console.WriteLine("результирующий массив: ");
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.Write(arrey3[i,j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
