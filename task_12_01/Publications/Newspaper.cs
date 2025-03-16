8af1010f0d2dede6456c773d7f5f688bf9a8ab0d
mukashova8 <alisha_mukashova_056@mail.ru>
2025-03-16 13:57:34 +0500

    задача 1 урока 12

---
+++ task_12_01/Publications/Newspaper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_12_01.Publications
+{
+    internal class Newspaper
+    {
+    }
+}
