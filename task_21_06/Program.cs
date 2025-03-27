f3bc440f1e4b5dc4151a68fd08f75ff18807c04f
mukashova8 <alisha_mukashova_056@mail.ru>
2025-03-27 15:00:48 +0500

    задачи 21 урока

---
+++ task_21_06/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace task_21_06
+{
+    internal class Program
+    {
+        //Написать метод, который удаляет повторяющиеся элементы из списка, сохраняя порядок.
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>
+            {
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 1, 2, 3, 4, 5
+            };
+            foreach (int number in numbers)
+            {
+                Console.Write(number + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Чистка повторяющихся элементов: ");
+            numbers = RemoveEl(numbers);
+            foreach (int number in numbers)
+            {
+                Console.Write(number + " ");
+            }
+        }
+        static List<int> RemoveEl(List<int> numbers)
+        {
+            List<int> newnumbers = new List<int>();
+            foreach (int number in numbers)
+            {
+                if (!newnumbers.Contains(number))
+                    newnumbers.Add(number);
+            }
+            return newnumbers;
+        }
+    }
+}
