28ff3d3d3b8f9b98f715e3e9fc1d9fcb3731c162
mukashova8 <alisha_mukashova_056@mail.ru>
2025-04-02 23:26:00 +0500

    Задачи 21 урока

--- task_21_08/Program.cs
+++ task_21_08/Program.cs
@@ -1,10 +1,47 @@
-﻿namespace task_21_08
+﻿using System.Net.Sockets;
+using Microsoft.VisualBasic;
+
+namespace task_21_08
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            //
+            //напишите метод, который на вход получается массив параметров (строк)
+            //и возвращает только уникальные строки
+
+            List<string> list = new List<string>()
+            {
+                "Ананас",
+                "Банан",
+                "Дыня",
+                "Ананас",
+                "Банан",
+                "Киви"
+            };
+            string[] uniqueStrings = TestString(list);
+            Print(uniqueStrings);
+        }
+        static string[] TestString(List<string> list)
+        {
+            var list2 = new List<string>();
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (!list2.Contains(list[i]))
+                {
+                    list2.Add(list[i]);
+                }
+            }
+            return list2.ToArray();
+        }
+
+        public static void Print(string[] uniqueStrings)
+        {
+            for (int i = 0; i < uniqueStrings.Length; i++)
+            {
+                Console.WriteLine(uniqueStrings[i]);
+            }
         }
     }
 }
